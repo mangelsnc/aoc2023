3abee178127afdc14b0b14300a9fb470a8250a76
Miguel Ángel Sánchez Chordi <mangel.snc@gmail.com>
2023-12-09 18:57:52 +0100

    Day 9

---
+++ 9/2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import re
+
+file = open('input.txt', 'r')
+
+total = 0
+for line in file:
+    history = [int(i) for i in re.findall(r"\-?\d+", line.strip())]
+   
+    step = 0
+    steps = [history]
+
+    while(any(steps[step]) == True):
+        new_history = []
+        for i in range(0, len(steps[step]) - 1):
+            new_history.append(steps[step][i + 1] - steps[step][i])
+        step += 1
+        steps.append(new_history)
+    previous_step = None
+    for step in reversed(steps):
+        if previous_step == None:
+            step.insert(0, 0)
+        else:
+            step.insert(0, step[0] - previous_step[0])
+        previous_step = step
+    
+    predicted_value = step[0]
+    total += predicted_value
+    
+print("Total final: %d" % total)
+file.close()
